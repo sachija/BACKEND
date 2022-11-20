f449f137c387424f39557cf7c3209ac922eb9317
sachija <107698865+sachija@users.noreply.github.com>
2022-11-20 15:40:30 +0530

    Update home.js

--- Route/home.js
+++ Route/home.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
 const app = express();
-const data7 = require("../Controller/home");
+const data6= require("../Controller/home");
 
 
 const dataRouter = express.Router();
-dataRouter.route('/home').get(data7.homeData)
+dataRouter.route('/home').get(data6.homeData)
 
 module.exports = dataRouter;
